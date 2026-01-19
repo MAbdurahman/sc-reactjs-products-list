995ed81c323212d51654f25ce88e0f1486aebc12
Mahdi Abdurrahman <mdbdrrhm@gmail.com>
2026-01-19 05:47:38 -0600

    add src/assets/links/* and src/assets/products/*

---
+++ src/assets/links/links.js
@@ -0,0 +1,13 @@
+
+
+export const centerLinks = [
+   {page: 'Home', href: '/home'},
+   {page: 'About', href: '/about'},
+   {page: 'Products', href: '/products'},
+   {page: 'Contact', href: '/contact'}
+];
+
+export const rightLinks = [
+   {page: 'Cart', href: '/cart'},
+   {page: 'Get Started', href: '/sign-in'}
+];
