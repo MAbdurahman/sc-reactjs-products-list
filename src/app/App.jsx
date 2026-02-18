e35b7e16e10c2ea10fec3631435c978cb007c5a0
Mahdi Abdurrahman <mdbdrrhm@gmail.com>
2026-02-18 12:41:38 -0600

    Remove extraneous code src/app/App.jsx

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,4 +1,4 @@
-import React, {Fragment, useEffect, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {Route, Routes} from 'react-router-dom';
 import {Slide, ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
